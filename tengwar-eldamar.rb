abe1ca22581fe7c106c393bd2d1bc000e983a5d8
Jude Vais <jvais@akamai.com>
2024-03-14 09:25:21 -0400

    initial commit

---
+++ tengwar-eldamar.rb
@@ -0,0 +1,19 @@
+cask "font-envy-code-r-nerd-font" do
+    version ":latest"
+    sha256 "3851e51ac58687c3f19da2f59aa3b19db49f2abaade33a3d883d73c4b7e5ec5f"
+  
+    url "https://at.boktypografen.se/Downloads/TengwarEldamar_3.zip"
+    name "Tengwar Eldamar"
+    desc "Fonts for transcribing Tolkiens Tengwar"
+    homepage "https://at.boktypografen.se/tengwareldamar.htm"
+  
+    livecheck do
+      url :url
+      strategy :github_latest
+    end
+  
+    font "tengel__.ttf"
+    font "tengel_a.ttf"
+  
+    # No zap stanza required
+  end
