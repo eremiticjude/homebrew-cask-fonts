9318df5b1c8d2cc1656490dc78263aeaf90ab88c
Jude Vais <jvais@akamai.com>
2024-03-14 10:15:39 -0400

    fixing various things

--- Casks/font-bpdots.rb
+++ Casks/font-bpdots.rb
@@ -1,6 +1,6 @@
 cask "font-bpdots" do
     version ":latest"
-    sha256 "e01cadabfb202153388ba9bb0be966e326db418e1281c19b947b5faa360ef6c3"
+    sha256 "9bb891cf03ad5e05354a2811aba2a2af8253ae7a50ef7f4e901fa9c62989745b"
   
     url "https://github.com/eremiticjude/cask-font-storage/raw/main/BPdots.zip"
     name "BPdots"
--- Casks/font-panic-sans.rb
+++ Casks/font-panic-sans.rb
@@ -2,7 +2,7 @@ cask "font-panic-sans" do
     version ":latest"
     sha256 "08dce9c7cf84cf2ba54d339453c6f6bf73f535f67b7e223ad79c9740abbcadbc"
   
-    url "https://github.com/eremiticjude/cask-font-storage/raw/main/okuda.zip"
+    url "https://github.com/eremiticjude/cask-font-storage/raw/main/PanicSans.zip"
     name "Panic Sans"
     desc "Panic company's custom fonts"
     homepage "https://panic.com/"
